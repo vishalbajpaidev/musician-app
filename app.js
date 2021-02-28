c1a025f009b36c26cdd941198709d4c2a3f6406c
Vishal Bajpai <vishal.dev1223@gmail.com>
2021-02-28 22:18:49 +0530

    Update app.js
    8080

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const Musician = require('./models/musician');
 const musicianRoutes = require('./routes/musician');
 
 const app = express();
-const port = 3001;
+const port = 8080;
 
 // include routes
 app.use('/musician', musicianRoutes);
